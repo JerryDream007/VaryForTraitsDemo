6db7a349d90c19df1dd84ef223ca7445162c146b
宋澎 <songpeng@songpengdemac-mini.local>
2018-04-28 11:28:33 +0800

    Initial Commit

---
+++ VaryForTraitsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VaryForTraitsDemo
+//
+//  Created by 宋澎 on 2018/4/28.
+//  Copyright © 2018年 宋澎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VaryForTraitsDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VaryForTraitsDemo
+//
+//  Created by 宋澎 on 2018/4/28.
+//  Copyright © 2018年 宋澎. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
